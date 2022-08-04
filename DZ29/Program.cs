7165b3ab39103abe653d0853e3d78db9aae184cd
Nikolay <cccp_ussr@bk.ru>
2022-08-04 06:57:04 +0300

    added DZ29

---
+++ DZ29/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] array = GetRandomArray(8);
+WriteArrayToConsole(array);
+
+int [] GetRandomArray(int arrayLength)
+{
+    Random rnd = new Random();
+    int[] arr = new int[arrayLength];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0,100);
+    }
+    return arr;
+}
+
+void WriteArrayToConsole(int[] array)
+{
+    Console.Clear();
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if(i != array.Length - 1)
+            Console.Write(",");
+    }
+    Console.WriteLine("]");
+}
