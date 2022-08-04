23c344c5e238d1fede9fa876ddfebae33e6a7015
Nikolay <cccp_ussr@bk.ru>
2022-08-04 06:24:55 +0300

    added DZ27

---
+++ DZ27/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int GetSum(int number)
+{
+    int sum = 0;
+    while(number >= 1)
+    {
+        sum = sum + number % 10;
+        number = number / 10;
+    }
+    return sum;
+}
+int result = GetSum(num);
+Console.WriteLine($"{num} -> {result}");
